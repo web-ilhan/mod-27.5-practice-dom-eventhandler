b214ba67b151450935302404a1ca14c563c7bc18
ILhankhondaker <ilhankhondaker@gmail.com>
2022-02-12 10:04:09 +0600

    f

---
+++ penguin.js
@@ -0,0 +1,10 @@
+document.getElementById('btn').addEventListener('click', function () {
+    console.log('ok button in now active')
+    const heading = document.getElementById('head')
+
+    // heading.innerText = "I AM PENGUIN"
+    heading.innerText = "Thank's For Yor Order"
+})
+
+
+onkeydown
