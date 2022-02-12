f13825c2faa1bf4f69a6ff02adc7ac7cc7b4ce02
ILhankhondaker <ilhankhondaker@gmail.com>
2022-02-12 14:16:47 +0600

    f

--- baper-bank/js/practice.js
+++ baper-bank/js/practice.js
@@ -1,13 +1,6 @@
-function doubleIt(num) {
-    const result = num * 2;
-    return result;
-}
-const fiveDouble = doubleIt(5);
-const severDouble = doubleIt(7);
-
 // ইনপুটফিল্ড থেকে মান কালেক্ট করার ফাংশন 
 function getInputValue(inputId) {
-    // debug korar jonno use kore 
+    // debug korar jonno use 
     // debugger;
     const inputField = document.getElementById(inputId);
     const inputAmountText = inputField.value;
@@ -36,9 +29,6 @@ function getCurrentBalance() {
 
 function updateBalance(amount, isAdd) {
     const balanceTotal = document.getElementById('balance-total');
-    /*
-     const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText); */
     const previousBalanceTotal = getCurrentBalance();
     if (isAdd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount;
@@ -49,27 +39,7 @@ function updateBalance(amount, isAdd) {
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
-    /* 
-    const depositInput = document.getElementById('deposit-input');
-    const depositAmountText = depositInput.value;
-    const depositAmount = parseFloat(depositAmountText); */
 
-
-    // get and update deposit total
-    /*  
-    const depositTotal = document.getElementById('deposit-total');
-     const depositTotalText = depositTotal.innerText;
-     const previousDepositTotal = parseFloat(depositTotalText);
- 
-     depositTotal.innerText = previousDepositTotal + depositAmount; */
-
-
-    // update balance
-    /* 
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-    balanceTotal.innerText = previousBalanceTotal + depositAmount; */
     const depositAmount = getInputValue('deposit-input');
     if (depositAmount > 0) {
         updateTotalField('deposit-total', depositAmount);
@@ -79,28 +49,6 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
 // handle withdraw button 
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    /*     
-    const withdrawInput = document.getElementById('withdraw-input');
-        const withdrawAmountText = withdrawInput.value;
-        const withdrawAmount = parseFloat(withdrawAmountText); */
-
-
-    // get and update withdraw total
-    /* 
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const previousWithdrawTotalText = withdrawTotal.innerText;
-    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
-
-    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount; */
-
-
-    // update balance after withdraw
-    /* 
-        const balanceTotal = document.getElementById('balance-total');
-        const balanceTotalText = balanceTotal.innerText;
-        const previousBalanceTotal = parseFloat(balanceTotalText);
-    
-        balanceTotal.innerText = previousBalanceTotal - withdrawAmount; */
 
     const withdrawAmount = getInputValue('withdraw-input');
     const currentBalance = getCurrentBalance();
